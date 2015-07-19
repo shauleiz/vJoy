ab7982a8be1a4d7e1008f30847778b49539046d5
Shaul <shaul.eiz@gmail.com>
2015-07-19 12:50:27 +0300

    Just testing - no real change

--- apps/SignApps.bat
+++ apps/SignApps.bat
@@ -1,4 +1,4 @@
-REM Sign applications if certification utility exists
+REM Sign applications (if certification utility exists)
 IF NOT EXIST %USERPROFILE%\DESKTOP\DigiCertUtil.exe GOTO NOUTIL
 %USERPROFILE%\DESKTOP\DigiCertUtil.exe sign .\vJoyConf\objfre_wlh_amd64\amd64\vJoyConf.exe*.\vJoyConf\objfre_wxp_x86\i386\vJoyConf.exe*.\vJoyFeeder\objfre_wlh_amd64\amd64\vJoyFeeder.exe*.\vJoyFeeder\objfre_wxp_x86\i386\vJoyFeeder.exe*.\External\JoyMonitor\x86\JoyMonitor.exe*.\External\JoyMonitor\x64\JoyMonitor.exe
 :NOUTIL
