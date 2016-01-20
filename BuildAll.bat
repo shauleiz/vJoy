382f86ba3150399a2122751987c8a848c7545294
Shauleiz <shaul.eiz@gmail.com>
2016-01-20 08:39:33 +0200

    Administrative
    Added Date/Time to the build batch file

--- BuildAll.bat
+++ BuildAll.bat
@@ -8,50 +8,50 @@ SET InnoCompiler=%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe
 
 
 :build32
-echo Cleaning vJoy (x86)
+echo %DATE% %TIME%: Cleaning vJoy (x86) 
 "%BUILDER%"  vjoy.sln  /maxcpucount:1 /t:clean /p:Platform=Win32;Configuration=Release
 set BUILD_STATUS=%ERRORLEVEL%
 if not %BUILD_STATUS%==0 goto fail
 
-echo Building vJoy (x86)
+echo %DATE% %TIME%: Building vJoy (x86)
 "%BUILDER%"  vjoy.sln  /maxcpucount:4  /p:Platform=Win32;Configuration=Release
 set BUILD_STATUS=%ERRORLEVEL%
 if not %BUILD_STATUS%==0 goto fail
 
 :build64
-echo Cleaning vJoy (x64)
+echo %DATE% %TIME%: Cleaning vJoy (x64)
 "%BUILDER%"  vjoy.sln  /maxcpucount:1 /t:clean /p:Platform=x64;Configuration=Release
 set BUILD_STATUS=%ERRORLEVEL%
 if not %BUILD_STATUS%==0 goto fail
 
-echo Building vJoy (x64)
+echo %DATE% %TIME%: Building vJoy (x64)
 "%BUILDER%"  vjoy.sln  /maxcpucount:4  /p:Platform=x64;Configuration=Release
 set BUILD_STATUS=%ERRORLEVEL%
 if not %BUILD_STATUS%==0 goto fail
 
 :signapps
-echo Signing the applications
+echo %DATE% %TIME%: Signing the applications
 IF NOT EXIST %DigiCertUtil% GOTO NOUTIL
 %DigiCertUtil% sign /sha1 "6d 54 71 df 6b bf af a5 d5 90 0c 88 c0 8d f0 e9 c5 13 69 0a" /noInput .\%Target64%\vJoyConf.exe*.\%Target32%\vJoyConf.exe*.\%Target64%\vJoyFeeder.exe*.\%Target32%\vJoyFeeder.exe*.\apps\External\JoyMonitor\x86\JoyMonitor.exe*.\apps\External\JoyMonitor\x64\JoyMonitor.exe
 set SIGN_STATUS=%ERRORLEVEL%
 if not %SIGN_STATUS%==0 goto fail
-echo Signing the applications - OK
+echo %DATE% %TIME%: Signing the applications - OK
 
 :inno
-echo Compiling the Inno Setup Script
+echo %DATE% %TIME%: Compiling the Inno Setup Script
 IF NOT EXIST "%InnoCompiler%" GOTO NOINNO
 "%InnoCompiler%" install\vJoyInstallerSigned.iss 
 set INNO_STATUS=%ERRORLEVEL%
 if not %INNO_STATUS%==0 goto fail
-echo Compiling the Inno Setup Script - OK
+echo %DATE% %TIME%: Compiling the Inno Setup Script - OK
 exit /b 0
 
 :NOUTIL
-echo Could not find DigiCertUtil on the desktop
+echo %DATE% %TIME%: Could not find DigiCertUtil on the desktop
 goto fail
 
 :NOINNO
-echo Could not find Inno Setup Compiler
+echo %DATE% %TIME%: Could not find Inno Setup Compiler
 goto fail
 
 :fail
