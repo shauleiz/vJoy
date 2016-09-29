969b95c74fa3f8663114c246353d2d75cb587177
Shaul <shauleiz@users.noreply.github.com>
2016-09-29 12:40:18 +0300

    Create Latest.php

---
+++ Latest.php
@@ -0,0 +1,6 @@
+<?php
+/* Redirect browser */
+header("Location: http://sourceforge.net/projects/vjoystick/files/Beta%202.x/SDK/vJoy217SDK-260916.zip/download");
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
