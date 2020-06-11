6ba2c7b0e718bcc9e8377ec985ddf66287e36dd9
Benjamin Maurin <benjamin.maurin@gmail.com>
2020-06-11 21:57:35 +0200

    - Added new files and remove temporary ones

---
+++ inc/gen-versioninfo.h
@@ -0,0 +1,13 @@
+#pragma once
+// Version as four nibbles as X.H.M.L
+#define VER_X_ 2
+#define VER_H_ 2
+#define VER_M_ 1
+#define VER_L_ 0
+// Internal use only
+#define BUILD 5
+
+#define FILEVER				"2, 2, 1, 0"
+#define PRODVER_TAG			"v2.2.1.0"
+#define PRODVER_SHA1		"742244"
+
