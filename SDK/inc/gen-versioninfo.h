e00cce0a956ec5a14b50d51da84a5995bb670f04
Benjamin Maurin <benjamin.maurin@gmail.com>
2020-06-14 00:05:31 +0200

    Update SDK files

--- SDK/inc/gen-versioninfo.h
+++ SDK/inc/gen-versioninfo.h
@@ -5,9 +5,9 @@
 #define VER_M_ 1
 #define VER_L_ 0
 // Internal use only
-#define BUILD 5
+#define BUILD 0
 
 #define FILEVER				"2, 2, 1, 0"
 #define PRODVER_TAG			"v2.2.1.0"
-#define PRODVER_SHA1		"c50f79"
+#define PRODVER_SHA1		"0a889e"
 
