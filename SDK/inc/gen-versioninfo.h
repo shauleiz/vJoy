7f21096386817a517fb9005af76d4aaded197d58
Benjamin Maurin <benjamin.maurin@gmail.com>
2020-06-28 10:47:15 +0200

    SDK: Increase version to v2.2.1.1, now uses .Net 4.5.2

--- SDK/inc/gen-versioninfo.h
+++ SDK/inc/gen-versioninfo.h
@@ -3,11 +3,11 @@
 #define VER_X_ 2
 #define VER_H_ 2
 #define VER_M_ 1
-#define VER_L_ 0
+#define VER_L_ 1
 // Internal use only
 #define BUILD 0
 
-#define FILEVER				"2, 2, 1, 0"
-#define PRODVER_TAG			"v2.2.1.0"
-#define PRODVER_SHA1		"5b04ae"
+#define FILEVER				"2, 2, 1, 1"
+#define PRODVER_TAG			"v2.2.1.1"
+#define PRODVER_SHA1		"d88d74"
 
